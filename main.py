411391ce0ba4176c17d77ad6138876ba340bb1fb
v1asavi <124231657+v1asavi@users.noreply.github.com>
2023-12-29 10:50:55 +0530

    main.py

--- main.py
+++ main.py
@@ -5,4 +5,32 @@ st.write("<h1>Hello world</h1>",unsafe_allow_html=True)
 #ing colour of test
 st.write("<h1ss style='color:red;'>hello world</h1>",unsafe_allow_html=True)
 st.file_uploader("upload file")
+import streamlit as st
+
+st.write("<h1>Hello Priya</h1>",unsafe_allow_html=True)
+
+#apply style
+st.write("<h1 style='color:blue;'>Hello Priya</h1>",unsafe_allow_html=True)
+
+#upload file
+st.file_uploader("upload file")
+
+#upload image
+st.image("https://img.freepik.com/free-photo/beautiful-rose-nature_23-2150737339.jpg")
+
+#upload video
+st.video("https://www.youtube.com/watch?v=DBlMnnuVBO4")
+
+#taking an image as input and displaying it
+img=st.file_uploader("upload image")
+st.image(img)
+
+
+
+
+
+
+
+
+
 
