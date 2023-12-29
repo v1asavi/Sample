a64cd0f4bc7714d79035794be05df60718234781
v1asavi <124231657+v1asavi@users.noreply.github.com>
2023-12-29 14:40:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,4 @@
 import streamlit as st
-st.write("Welcome to my app")
-import streamlit as st
-st.write("<h1>Hello world</h1>",unsafe_allow_html=True)
-#ing colour of test
-st.write("<h1ss style='color:red;'>hello world</h1>",unsafe_allow_html=True)
-st.file_uploader("upload file")
-import streamlit as st
-
-st.write("<h1>Hello snehitha</h1>",unsafe_allow_html=True)
-
-#apply style
-st.write("<h1 style='color:blue;'>Hello snehitha</h1>",unsafe_allow_html=True)
-
-#upload file
-st.file_uploader("upload file")
-
-#upload image
-st.image("https://img.freepik.com/free-photo/beautiful-rose-nature_23-2150737339.jpg")
-
-#upload video
-st.video("https://www.youtube.com/watch?v=DBlMnnuVBO4")
-
-#taking an image as input and displaying it
-img=st.file_uploader("upload image")
-st.image(img)
-import streamlit as st
 import numpy as np
 
 st.set_page_config(layout ="wide")
@@ -36,14 +10,3 @@ rollnumber=st.sidebar.number_input("Roll Number")
 name=st.sidebar.text_input("Name")
 fees=st.sidebar.number_input("Fees")
 add=st.sidebar.button("Add")
-
-
-
-
-
-
-
-
-
-
-
