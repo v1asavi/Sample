80771bafa61f851f2258a7a88e7cc6ff62eb3869
v1asavi <124231657+v1asavi@users.noreply.github.com>
2023-12-29 10:19:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ st.write("<h1>Hello world</h1>",unsafe_allow_html=True)
 #ing colour of test
 st.write("<h1ss style='color:red;'>hello world</h1>",unsafe_allow_html=True)
 st.file_uploader("upload file")
+
