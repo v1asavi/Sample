1e6d95204b4ceb0f96eb678055f519ee1d969a1b
v1asavi <124231657+v1asavi@users.noreply.github.com>
2023-12-29 15:12:12 +0530

    main.py

--- main.py
+++ main.py
@@ -6,8 +6,17 @@ st.set_page_config(layout ="wide")
 st.title("Student Management System")
 st.sidebar.title("Fee Management System")
 
-st.sidebar.subheader("Add Student")
-rollnumber=st.sidebar.number_input("Roll Number")
-name=st.sidebar.text_input("Name")
-fees=st.sidebar.number_input("Fees")
-add=st.sidebar.button("Add")
+st.sidebar.subheader("Menu")
+radio_value = st.sidebar.radio("Choose an option:", ["add student", "delete student", "view fee","pay fee"])
+st.write("You selected:", radio_value)
+if radio_value =="add student":
+  rollnumber=st.number_input("Enter student number")
+  name=st.text_input("Name")
+  fees=st.number_input("Fees")
+  add=st.button("Add Student")
+elif radio_value == "delete student":
+  delete=st.button("delete record")
+elif radio_value =="view fee":
+  view=st.button("view fee")
+elif radio_value=="pay fee":
+  Pay=st.button("Pay")
